e3c835988a0f83afad75bccb7430290e792734ad
Francisco Souza <f@souza.cc>
2012-05-20 01:11:46 -0300

    srvdir: added -http flag
    With this flag, you can choose which port to listen, or even the host.
    
    Examples:
    
        % srvdrv -http=:80
        % srvdrv -http=127.0.0.1:8000

--- srvdir.go
+++ srvdir.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"flag"
 	"net/http"
 )
 
+var port string
+
+func init() {
+	flag.StringVar(&port, "http", ":9090", "port to be used by the server")
+}
+
 func main() {
+	flag.Parse()
 	http.Handle("/", http.FileServer(http.Dir("")))
-	if err := http.ListenAndServe(":9090", nil); err != nil {
+	if err := http.ListenAndServe(port, nil); err != nil {
 		panic(err)
 	}
 }
