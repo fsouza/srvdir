123d129bee0e95af633b5b203ec00a662c5ce308
Francisco Souza <f@souza.cc>
2016-02-12 14:34:25 -0200

    Add cors headers

--- srvdir.go
+++ srvdir.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2013, Francisco Souza
+// Copyright (c) 2016, Francisco Souza
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
@@ -37,9 +37,18 @@ func init() {
 	flag.StringVar(&port, "http", ":9090", "port to be used by the server")
 }
 
+func corsMiddleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, api_key, Authorization")
+		h.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	flag.Parse()
-	http.Handle("/", http.FileServer(http.Dir("")))
+	http.Handle("/", corsMiddleware(http.FileServer(http.Dir(""))))
 	log.Printf("Running at http://127.0.0.1%s", port)
 	if err := http.ListenAndServe(port, nil); err != nil {
 		log.Fatal(err)
